b218cf6ac6d04777d6c9875f653db021a116e992
Helder Boone Borges <Helder.Borges@ey.com>
2023-03-08 15:14:26 -0300

    Creating the aditional projects

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,10 @@
-using API.Data;
+using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(options =>
@@ -16,7 +14,6 @@ builder.Services.AddDbContext<StoreContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
