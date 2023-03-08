f07c68a4faefb4560f299e8d3a11a3b4c2f15045
Helder Boone Borges <Helder.Borges@ey.com>
2023-03-08 15:21:47 -0300

    Adding a repository interface

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
